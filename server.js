1eb171c305b4ac0a3df459f9faa9a6712fe0071c
kasrarh <55912648+kasrarh@users.noreply.github.com>
2022-10-02 13:15:33 -0400

    assignment 2

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
-
+var express = require("express")
+var app = express()
+const path = require("path")
 // setup a 'route' to listen on the default url path
-app.get("/",function (req,res){
-    res.send("Home Page")
-})
-// setup http server to listen on HTTP_PORT
-var port = process.env.PORT || 3000;
-app.listen(HTTP_PORT);
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/blog.html"))
+  })
+  app.get("/Blog", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/blog.html"))
+  })
+  app.get("/article/:id", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/article.html"))
+  })
+  app.get("/registration", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+  })
+  app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/login.html"))
+  })
+var port = process.env.PORT || 3000
+app.listen(port)
