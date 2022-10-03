7b4067e78b706435359e761a31de66382648eb28
kasrarh <55912648+kasrarh@users.noreply.github.com>
2022-10-02 22:46:52 -0400

    assignment 2 done

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-var express = require("express")
-var app = express()
-const path = require("path")
-// setup a 'route' to listen on the default url path
+var port = process.env.PORT || 3000; // our url is now -> http://localhost:3000
+
+const exp = require("express");
+const app = exp();
+const path = require("path");
+
+app.use("/pictures", exp.static(path.join(__dirname, "pictures")));
+
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/blog.html"))
-  })
-  app.get("/Blog", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/blog.html"))
-  })
-  app.get("/article/:id", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/article.html"))
-  })
-  app.get("/registration", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/register.html"))
-  })
-  app.get("/login", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/login.html"))
-  })
-var port = process.env.PORT || 3000
-app.listen(port)
+  res.sendFile(path.join(__dirname, "blog.html"));
+});
+app.get("/Blog", function (req, res) {
+  res.sendFile(path.join(__dirname, "blog.html"));
+});
+app.get("/article/:id", function (req, res) {
+  res.sendFile(path.join(__dirname, "article.html"));
+});
+app.get("/registration", function (req, res) {
+  res.sendFile(path.join(__dirname, "register.html"));
+});
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
+
+app.listen(port);
