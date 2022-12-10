d60d72e352e3f64da06bbd9f18a1a4cbad3ce96b
kasrarh <55912648+kasrarh@users.noreply.github.com>
2022-12-09 20:20:58 -0500

    assignment 6

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 Name: Kasra Rahimian
 Email : krahimian@myseneca.ca
 studentId: 118335207
-Assignemnt3
-Date: Oct 19th 2022
+Assignemnt6
+Date: Dec 9th 2022
  Cylic URL https://lively-gear-bear.cyclic.app
  Github : kasrarh/WEB322
 */
@@ -15,25 +15,159 @@ const app = exp();
 const path = require("path");
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+const upload = multer();
+
+mongoose.connect(
+  "mongodb+srv://krahimian:251174kr@cluster1.fyydvdc.mongodb.net/hostingCanada?retryWrites=true&w=majority"
+);
+
+var companySchema = new Schema({
+  "companyName":  String,
+  "address": String,
+  "phone": String,
+  "employeeCount": {
+    "type": Number,
+    "default": 0
+  },
+  "country": String  
+});
+
+var Company = mongoose.model("web322_companies", companySchema);
 
 function specialChars(data) {
     const chars = /[ `~!@#$%^&*()_+=\-?]/;
     return chars.test(data)
   }
 
+  
+let users = mongoose.model("users", users_schema);
+let blogs = mongoose.model("blogs");
+
 app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.use("/pictures", exp.static(path.join(__dirname, "pictures")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/", async function (req, res) {
+  let title = "Blog";
+  const results = await blogs.find({});
+  await results.forEach(console.dir);
+  for (let index = 0; index < results.length; index++) {
+    var finalResults = results[index]["description"];
+  }
+  res.render("blog", { layout: false, title: title, findResult2: findResult2 });
+});
 app.get("/", function (req, res) {
-    res.render("blog", { layout: false });
+  let title = "Blog";
+  var blogsArray = [];
+  blogs.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      blogsArray = blogs.map((blog) => ({
+        _id: blog._id,
+        title: blog.title,
+        description: blog.desc,
+        imgUrl: blog.imgUrl,
+        date: blog.date,
+      }));
+
+      res.render("blog", {
+        layout: false,
+        finalResult: blogsArray,
+        title: title,
+      });
+    }
+  });
 });
 app.get("/Blog", function (req, res) {
-    res.render("blog", { layout: false });
+  let title = "Blog";
+  var blogsArray = [];
+  blogs.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      blogsArray = blogs.map((blog) => ({
+        _id: blog._id,
+        title: blog.title,
+        description: blog.desc,
+        imgUrl: blog.imgUrl,
+        date: blog.date,
+      }));
+
+      res.render("blog", {
+        layout: false,
+        finalResult: blogsArray,
+        title: title,
+      });
+    }
+  });
 });
-app.get("/article/:id", function (req, res) {
-    res.render("article", { layout: false });
+app.get("/article/:id", async function (req, res) {
+  let title = "Article";
+
+  const results = await blogs.find({});
+  await results.forEach(console.dir);
+  let blogTitle = results[0]["title"];
+  let blogDescription = results[0]["description"];
+
+  res.render("article", {
+    title: title,
+    layout: false,
+    blogTitle: blogTitle,
+    blogDescription: blogDescription,
+  });
+});
+app.get("/editArticle/:_id", (req, res) => {
+  const id = req.params._id;
+  blogs.findOne({ _id: id }, (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("editArticle", {
+        layout: false,
+        title: posts.title,
+        _id: posts._id,
+        description: posts.description,
+        imgUrl: posts.imgUrl,
+      });
+    }
+  });
+});
+
+app.post("editArticle/:_id", function (req, res) {
+  const id = req.params._id;
+  let title = req.body.title;
+  let description = req.body.description;
+  let imgUrl = req.body.imgUrl;
+  const data = {
+    title: title,
+    desc: desc,
+    imgName: imgName,
+    imgUrl: imgUrl,
+  };
+  let updatedArticle = blogs.findOneAndUpdate(
+    { _id: id },
+    data,
+    { new: true },
+    (err) => {
+      if (err) console.log("Error has been found");
+      else {
+        app.redirect("/articles");
+      }
+    }
+  );
+  updatedArticle
+    .save()
+    .then(() => {
+      res.redirect("articles");
+    })
+    .catch((err) => {
+      console.log("There was an error updating the new Article");
+    });
 });
 app.get("/login", function (req, res) {
     res.render("login", { layout: false });
@@ -70,5 +204,7 @@ app.post("/register", function (req, res) {
             res.render("register", { layout: false, errorMessage: errorMessage, data:body });
          }
 })
+
+
 
 app.listen(port);
